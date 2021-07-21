017201fce3e06e779913db07ccbe12fc2812ac50
Michael <michaeledwards2009@gmail.com>
2021-07-21 11:29:04 -0400

    remove unused media query

--- src/components/ModalComponents/ModalCard.js
+++ src/components/ModalComponents/ModalCard.js
@@ -13,15 +13,6 @@ const ModalCard = styled.div`
   border-radius: 10px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 
-  @media (max-width: 250px) {
-    margin: 0;
-    border-radius: 0;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
-
   @media screen and (max-width: 600px) {
     width: 100%;
     height: 100%;
