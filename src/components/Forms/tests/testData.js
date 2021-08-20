18303c23410f7f0d1b7fd77de9acf0d0c2fd13a0
Michael <michaeledwards2009@gmail.com>
2021-08-20 16:06:32 -0400

    add unit tests for sign in form

--- src/components/Forms/tests/testData.js
+++ src/components/Forms/tests/testData.js
@@ -13,3 +13,30 @@ export const successfullSubmit = {
   message:
     "An email has been sent to your inbox. Click the link the the email to verify your account",
 };
+
+export const successfullSignIn = {
+  data: {
+    success: true,
+    token: "sldkjfno3wi4ernoifnersdmnfow3eih",
+  },
+};
+
+export const unsuccessfullSignIn = {
+  status: 400,
+  response: {
+    data: {
+      success: false,
+      error: "Invalid credentials",
+    },
+  },
+};
+
+export const unverifiedAccount = {
+  status: 400,
+  response: {
+    data: {
+      success: false,
+      error: "Please follow the link in your email to verify your account",
+    },
+  },
+};
